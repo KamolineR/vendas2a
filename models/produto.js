896250bb01348e317738730acdc2bc4b2da44902
KamolineR <164529564+KamolineR@users.noreply.github.com>
2024-09-13 08:02:44 -0300

    Add files via upload

--- models/produto.js
+++ models/produto.js
@@ -10,7 +10,11 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Produto.belongsToMany(models.produto, {
+        foreignKey: 'ProdutoId', through: 'Item', as: 'produto_serv'});
+        Produto.hasMany(models.produto, {
+          foreignKey: 'ProdutoId'})
+      }
     }
   }
   Produto.init({
